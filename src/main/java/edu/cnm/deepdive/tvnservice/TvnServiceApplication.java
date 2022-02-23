c71f5b9b2ae23df2fb84e755d3f5822d070b82d4
Kyle Coleman <kcoleman20@cnm.edu>
2022-02-23 13:45:00 -0700

    Initial commit

---
+++ src/main/java/edu/cnm/deepdive/tvnservice/TvnServiceApplication.java
@@ -0,0 +1,13 @@
+package edu.cnm.deepdive.tvnservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TvnServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TvnServiceApplication.class, args);
+  }
+
+}
---
+++ src/test/java/edu/cnm/deepdive/tvnservice/TvnServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cnm.deepdive.tvnservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TvnServiceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
